7ca4d8a3c7d8c2674c69d357a6817c35a26739d1
Andy MacKay <binarygeometry@gmail.com>
2023-09-08 04:30:18 +0100

    Installing the d3 library and rendering some data

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,33 +2,74 @@ import React, { Component }  from 'react' ;
 
 import ValidationService from "../services/ValidationService";
 
+import * as d3 from 'd3';
 
 // CSS Styles for active Pad
 const defaultTheme = {
   backgroundColor: 'orange',
   color: '#f1fa8c'
 };
+
+interface IProps {
   
+}
 
-  const UserContext = React.createContext();
-// // CSS Style for inactive Pad
-// const inactivePad = {
-//   backgroundColor: '#44475a',
-//   color: '#8be9fd'
-// };
+interface IState {
+  
+}  
 
-//  Pad Component
-export default class Display extends Component {
+export default class Display extends Component<IProps, IState> {
 
   constructor(props){
     super(props);
     this.state = {
       padStyle : defaultTheme
     }
+	 this.ref = SVGSVGElement; 
     // this.display = 'Showing something';
     this.display = ValidationService.firstValidationMethod();
+	// this.ref = SVGSVGElement;  
+	
+
     // this.playClip = this.playClip.bind(this);
     // this.handleKeyDown =  this.handleKeyDown.bind(this);
+  }
+
+
+  buildGraph(data: Array<number>) {
+    const width = 200,
+    scaleFactor = 10,
+    barHeight = 20;
+
+    const graph = d3.select(this.ref)
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
+
+    const bar = graph.selectAll("g")
+      .data(data)
+      .enter()
+      .append("g")
+      .attr("transform", function(d, i) {
+            return "translate(0," + i * barHeight + ")";
+      });
+
+    bar.append("rect")
+      .attr("width", function(d) {
+                return d * scaleFactor;
+      })
+      .attr("height", barHeight - 1);
+       
+    bar.append("text")
+      .attr("x", function(d) { return (d*scaleFactor); })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
+    
+  }
+  
+  componentDidMount() {
+    // activate   
+    this.buildGraph([5, 10, 12]);
   }
   
   render(){
@@ -40,6 +81,8 @@ export default class Display extends Component {
         <h2>{this.display.display}</h2>
         <h2>{this.display.volume}</h2>
         <h2>{this.display.padBank}</h2>
+          <svg className="container" ref={(ref: SVGSVGElement) => this.ref = ref} width='100' height='100'></svg>
+    
       </div>
     );    
   }
